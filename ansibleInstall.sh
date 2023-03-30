25424ebf9f524dad98908fa93a327f202512ff26
mpiasix2 <129409812+mpiasix2@users.noreply.github.com>
2023-03-30 19:47:22 +0200

    Update ansibleInstall.sh

--- ansibleInstall.sh
+++ ansibleInstall.sh
@@ -3,8 +3,10 @@
 # Verificar si Ansible está instalado
 if ! which ansible >/dev/null; then
   echo "Instalando Ansible..."
-  sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
-  sudo apt install -y ansible
+  sudo apt install ansible -y
+  # Generar claves SSH
+  echo "Generando claves SSH..."
+  sudo ssh-keygen
 fi
 
 # Pedir las direcciones IP y los usuarios
@@ -12,7 +14,11 @@ read -p "Introduzca la dirección IP y el usuario de la máquina fw (ejemplo: us
 read -p "Introduzca la dirección IP y el usuario de la máquina sec (ejemplo: usuario@ip): " sec_info
 
 # Agregar las direcciones IP y los usuarios al archivo /etc/ansible/hosts
+sudo rmdir -rf /etc/ansible
+sudo mkdir /etc/ansible
 sudo rm /etc/ansible/hosts
+sudo touch /etc/ansible/hosts
+sudo echo "" > /etc/ansible/hosts
 echo "" | sudo tee -a /etc/ansible/hosts > /dev/null
 echo "[fw]" | sudo tee -a /etc/ansible/hosts > /dev/null
 echo "fw ansible_host=$fw_info" | sudo tee -a /etc/ansible/hosts > /dev/null
@@ -23,15 +29,9 @@ echo "" | sudo tee -a /etc/ansible/hosts > /dev/null
 echo "[all:vars]" | sudo tee -a /etc/ansible/hosts > /dev/null
 echo "ansible_python_interpreter=/usr/bin/python3" | sudo tee -a /etc/ansible/hosts > /dev/null
 
-# Generar claves SSH
-echo "Generando claves SSH..."
-ssh-keygen -t rsa
-
 # Copiar clave SSH a fw y sec
 echo "Copiando clave SSH a fw y sec..."
-ssh-copy-id -i ~/.ssh/id_rsa.pub fw_info
-ssh-copy-id -i ~/.ssh/id_rsa.pub sec_info
+sudo ssh-copy-id $(echo $fw_info | grep -o "@.*" | cut -c 2-)
+sudo ssh-copy-id $(echo $sec_info | grep -o "@.*" | cut -c 2-)
 
-echo "Las direcciones IP y los usuarios se han agregado correctamente al archivo /etc/ansible/hosts y la clave SSH se ha copiado a fw y sec."
-
-
+echo "Las direcciones IP y los usuarios se han agregado correctamente al archivo /etc/ansible/hosts junto a la clave SSH"
